26eeb2c8532e9111bee084cc021da5f6748cc763
Sagie501 <Sagie012@gmail.com>
2016-12-09 01:00:08 +0200

    Little fixes with signin and signup

--- OfekTwitter/public/javascript/signin.js
+++ OfekTwitter/public/javascript/signin.js
@@ -1,17 +1,44 @@
-$('#signInBtn').result[0].addEventListener('click', function () {
-    let username = $('#username').result[0];
-    let password = $('#password').result[0];
+$('#signInBtn').click(function () {
+    let username = $('#username');
+    let password = $('#password');
+    username.tooltip({
+        trigger: 'manual'
+    });
 
-    if (username !== "" && password !== "") {
-        loginToUserPromise({username: username.value, password: password.value}).then(function (res) {
+    password.tooltip({
+        trigger: 'manual'
+    });
+
+    username.focus(function () {
+        username.tooltip('hide');
+    });
+
+    password.focus(function () {
+        password.tooltip('hide');
+    });
+
+    if (validateUser(username, password)) {
+        loginToUserPromise({username: username.val(), password: password.val()}).then(function (res) {
                     sessionUser = res.data.sessionuser;
                     alert("Welcome back " + username.value + "!");
+                    username.val("");
+                    password.val("");
                     window.location = "/";
             }).catch(function () {
                 alert("Username or password not valid.");
-                password.value = "";
+                password.val("");
             });
     } else {
-        alert("Not all input forms filled.");
+        if (username.val() === "") {
+            username.tooltip('show');
+            password.tooltip('hide');
+        } else if (password.val() === "") {
+            password.tooltip('show');
+            username.tooltip('hide');
+        }
     }
 });
+
+function validateUser(username, password) {
+    return username.val() !== "" && password.val() !== "";
+}
--- OfekTwitter/public/javascript/signup.js
+++ OfekTwitter/public/javascript/signup.js
@@ -2,6 +2,8 @@ $("#signUpBtn").click(function () {
    let username = $("#username");
    let password = $("#password");
    let confirmPassword = $("#confirmPassword");
+    tooltipSettings(username, password, confirmPassword);
+
    if (validteInput(username.val(), password.val(), confirmPassword.val())) {
        createNewUserPromise({username: username.val(), password: password.val(), confirmPassword: confirmPassword.val()}).then(function (response) {
            if (response.data.result == true) {
@@ -15,22 +17,52 @@ $("#signUpBtn").click(function () {
            }
        });
    } else {
-       if (username.val() === "") {
-           username.tooltip('show');
-           password.tooltip('hide');
-           confirmPassword.tooltip('hide');
-       } else if (password.val() === "") {
-           password.tooltip('show');
-           username.tooltip('hide');
-           confirmPassword.tooltip('hide');
-       }else if (confirmPassword.val() === "") {
-           confirmPassword.tooltip('show');
-           username.tooltip('hide');
-           password.tooltip('hide');
-       }
+       tooltipHandler(username, password, confirmPassword);
    }
 });
 
 let validteInput = function(username, password, confirmPassword) {
     return username !== "" && password !== "" && confirmPassword !== "";
 };
+
+function tooltipSettings(username, password, confirmPassword) {
+    username.tooltip({
+        trigger: 'manual'
+    });
+
+    password.tooltip({
+        trigger: 'manual'
+    });
+
+    confirmPassword.tooltip({
+        trigger: 'manual'
+    });
+
+    username.focus(function () {
+        username.tooltip('hide');
+    });
+
+    password.focus(function () {
+        password.tooltip('hide');
+    });
+
+    confirmPassword.focus(function () {
+        confirmPassword.tooltip('hide');
+    });
+}
+
+function tooltipHandler(username, password, confirmPassword) {
+    if (username.val() === "") {
+        username.tooltip('show');
+        password.tooltip('hide');
+        confirmPassword.tooltip('hide');
+    } else if (password.val() === "") {
+        password.tooltip('show');
+        username.tooltip('hide');
+        confirmPassword.tooltip('hide');
+    } else if (confirmPassword.val() === "") {
+        confirmPassword.tooltip('show');
+        username.tooltip('hide');
+        password.tooltip('hide');
+    }
+}
